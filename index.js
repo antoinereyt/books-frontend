314158dd5e9f85cc72da4548293fa33e149181c5
Antoine Reyt <antoine.reyt@zeloce.com>
2020-07-05 14:51:15 +0200

    Fake feature

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World !!! v2.0.8'))
+app.get('/', (req, res) => res.send('Hello World !!! v2.0.10'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
