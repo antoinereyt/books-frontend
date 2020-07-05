e65f7dd774a1edc755239b6db51d9682790ced14
Antoine Reyt <antoine.reyt@zeloce.com>
2020-07-05 13:18:32 +0200

    Fake v2.0.8 feature with failing tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var expect = require('chai').expect;
 
 describe('Fake test()', function () {
   it('assert the truth', function () {
-    expect(1).to.be.equal(1);
+    expect(1).to.be.equal(2);
   });
 });
